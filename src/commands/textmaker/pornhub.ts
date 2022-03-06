afef62d598a04bda6295250969b10990af94ac91
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2022-03-06 09:23:07 +0200

    Update pornhub.ts

--- src/commands/textmaker/pornhub.ts
+++ src/commands/textmaker/pornhub.ts
@@ -11,11 +11,11 @@ const w5botapi = require('w5-textmaker');
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
-			command: "pornhub",
+			command: "valentine",
 			description: `Get text image`,
-			aliases: ["pornhub"],
+			aliases: ["valentine"],
 			category: "textmaker",
-			usage: `${client.config.prefix}pornhub`,
+			usage: `${client.config.prefix}valentine`,
 			baseXp: 50,
 		});
 	}
@@ -23,7 +23,7 @@ export default class Command extends BaseCommand {
 	run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
 		 if (!joined) return void (await M.reply(`Provide the text, Baka!`))
         const cara = joined.trim()
-		const wall = await w5botapi.textpro("https://textpro.me/pornhub-style-logo-online-generator-free-977.html",
+		const wall = await w5botapi.textpro("https://textpro.me/create-neon-light-on-brick-wall-online-1062.html",
     cara
     );
 		const buffer = await request.buffer(wall).catch((e) => {
