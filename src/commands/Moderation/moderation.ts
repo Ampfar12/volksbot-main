e6613172d3a98d9a0aea361d53805c510fa9d69f
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2022-03-06 10:40:01 +0200

    Update moderation.ts

--- src/commands/Moderation/moderation.ts
+++ src/commands/Moderation/moderation.ts
@@ -23,26 +23,52 @@ export default class Command extends BaseCommand {
         ]
         let bnh = n[Math.floor(Math.random() * n.length)]
         const mn=`
-Konnichiwa ${M.sender.username}
-    this is ${this.client.config.prefix}moderation
- Shows the moderation info
-   
-        「 moderation 」
- -「 ${this.client.config.prefix}activate : cmd , invitelink , nsfw , mod , events , safe 」
- -「 ${this.client.config.prefix}deactivate 」
- -「 ${this.client.config.prefix}close 」
- -「 ${this.client.config.prefix}demote 」
- -「 ${this.client.config.prefix}ping 」
- -「 ${this.client.config.prefix}gset 」
- -「 ${this.client.config.prefix}open 」
- -「 ${this.client.config.prefix}close 」
- -「 ${this.client.config.prefix}promote 」
- -「 ${this.client.config.prefix}purge 」
- -「 ${this.client.config.prefix}revoke 」
--「${this.client.config.prefix}add 」
- - 「${this.client.config.prefix}gadd desc 」
-- 「${this.client.config.prefix}gadd desc 」
+👋 (❤ω❤) Hello  ${M.sender.username}
 
+        「 moderation 」
+🔰Command:*activate*
+💠Description:
+♦️Usage:${this.client.config.prefix}activate : cmd , invitelink , nsfw , mod , events , safe
+
+🔰Command: *deactivate*
+💠Description:
+♦️Usage:${this.client.config.prefix}deactivate : : cmd , invitelink , nsfw , mod , events , safe
+
+🔰Command: *close*
+💠Description: closes the group
+♦️Usage:${this.client.config.prefix}close
+
+🔰Command: *demote*
+💠Description: removes someone from been admin
+♦️Usage:${this.client.config.prefix}demote (tag member)
+
+🔰Command:*gest*
+💠Description: sets group name
+♦️Usage:${this.client.config.prefix}gset (group name)
+
+🔰Command:*open*
+💠Description: opens group
+♦️Usage:${this.client.config.prefix}open
+
+🔰Command:*promote*
+💠Description: adds someone as admin
+♦️Usage:${this.client.config.prefix}promote (tag member)
+
+🔰Command:*purge*
+💠Description:removes everyone in the group
+♦️Usage:${this.client.config.prefix}purge
+
+🔰Command:*revoke*
+💠Description:reset group link
+♦️Usage:${this.client.config.prefix}revoke
+
+🔰Command:*add*
+💠Description:adds someone in group
+♦️Usage:${this.client.config.prefix}add (number)
+
+🔰Command:*desc*
+💠Description:sets group description
+♦️Usage:${this.client.config.prefix}gadd desc (group desciption)
 
   *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`
         return void this.client.sendMessage(M.from, { url: bnh }, MessageType.video, {quoted:M.WAMessage,
