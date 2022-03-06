96f90f1277e1a891002d76d36beae711a7e1e3da
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2022-03-06 03:58:24 +0200

    Update fun.ts

--- src/commands/Fun/fun.ts
+++ src/commands/Fun/fun.ts
@@ -26,18 +26,49 @@ export default class Command extends BaseCommand {
 👋 (❤ω❤) Hello ${M.sender.username}
 
            ꜰᴜɴ ꜱɪᴅᴇ 
- ${this.client.config.prefix}chat
- ${this.client.config.prefix}fact
- ${this.client.config.prefix}joke
- ${this.client.config.prefix}quote
- ${this.client.config.prefix}re
- ${this.client.config.prefix}ship
- ${this.client.config.prefix}trigger
- ${this.client.config.prefix}advice
- ${this.client.config.prefix}jail
- ${this.client.config.prefix}why
- ${this.client.config.prefix}chess
- ${this.client.config.prefix}igstalk (username) 
+🔰Command: *chat*
+💠Description: chat with bot in the group
+♦️Usage: ${this.client.config.prefix}chat
+
+🔰Command: *fact*
+💠Description: tells you random facts
+♦️Usage: ${this.client.config.prefix}fact
+
+🔰Command: *joke*
+💠Description:tells a random joke
+♦️Usage: ${this.client.config.prefix}joke
+
+🔰Command: *quote*
+💠Description: random quotes
+♦️Usage: ${this.client.config.prefix}quote
+
+🔰Command: *reactions*
+💠Description: shows reactions
+♦️Usage: ${this.client.config.prefix}react
+
+🔰Command: *ship*
+💠Description: ships you with some
+♦️Usage: ${this.client.config.prefix}ship (tag person)
+
+🔰Command: *trigger*
+💠Description: shakes your profile picture
+♦️Usage: ${this.client.config.prefix}trigger
+
+🔰Command: *advice*
+💠Description: sends random advice
+♦️Usage:${this.client.config.prefix}advice
+
+🔰Command: *jail*
+💠Description: takes you to jail
+♦️Usage:${this.client.config.prefix}jail
+
+🔰Command: *why*
+💠Description: sends random questions n responses
+♦️Usage: ${this.client.config.prefix}why
+
+🔰Command: *igstalk*
+💠Description: checks Instagram account
+♦️Usage:${this.client.config.prefix}igstalk (username)
 
   *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`
         return void this.client.sendMessage(M.from, { url: bnh }, MessageType.video, {quoted:M.WAMessage,
