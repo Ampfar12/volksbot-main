b9515efa4e29192b2c9c92c116e70d45d8a41547
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2022-03-06 08:02:26 +0200

    Update media.ts

--- src/commands/Media/media.ts
+++ src/commands/Media/media.ts
@@ -23,22 +23,52 @@ export default class Command extends BaseCommand {
         ]
         let bnh = n[Math.floor(Math.random() * n.length)]
         const mn=`
-Konnichiwa ${M.sender.username}
-    this is ${this.client.config.prefix}media
- Shows the media info
-   
-        「 Media 」
- -「 ${this.client.config.prefix}Blur 」
- -「 ${this.client.config.prefix}google 」
- -「 ${this.client.config.prefix}play 」
- -「 ${this.client.config.prefix}retrieve 」
--「 ${this.client.config.prefix}screenshot 」
- -「 ${this.client.config.prefix}advice 」
- -「 ${this.client.config.prefix}take : crop , stretch , author/pack 」
- -「 ${this.client.config.prefix}subred 」
- -「 ${this.client.config.prefix}yta 」
- -「 ${this.client.config.prefix}ytv 」
- -「 ${this.client.config.prefix}spotify 」
+👋 (❤ω❤) Hello ${M.sender.username}
+    
+           ᴍᴇᴅɪᴀ ꜱɪᴅᴇ
+🔰Command: *blur*
+💠Description: blurs a imagine
+♦️Usage:${this.client.config.prefix}Blur
+
+🔰Command: *google*
+💠Description: searches something on gooogle
+♦️Usage:${this.client.config.prefix}google (what you wanna google) 
+
+🔰Command: *play*
+💠Description: plays the song you want
+♦️Usage:${this.client.config.prefix}play (song name) 
+
+🔰Command: *retrieve*
+💠Description: send view once pick or video
+♦️Usage:${this.client.config.prefix}retrieve (make sure to tag a view once picture/video when using this command) 
+
+🔰Command: *screenshot*
+💠Description: screenshots 
+♦️Usage:${this.client.config.prefix}screenshot (link) 
+
+🔰Command: *advice*
+💠Description: gives random advice
+♦️Usage:${this.client.config.prefix}advice
+
+🔰Command: *steal*
+💠Description: steals a sticker
+♦️Usage:${this.client.config.prefix}steal : crop , stretch , author/pack
+
+🔰Command: *yts*
+💠Description: searches on YouTube
+♦️Usage:${this.client.config.prefix}yts (what you wanna search) 
+
+🔰Command: *yta*
+💠Description: confirms YouTube video to audio
+♦️Usage:${this.client.config.prefix}yta (link) 
+
+🔰Command: *ytv*
+💠Description: turns YouTube video to a video
+♦️Usage:${this.client.config.prefix}ytv (link) 
+
+🔰Command: *spotify*
+💠Description: searchs song on spotify 
+♦️Usage:${this.client.config.prefix}spotify (song name) 
  
   *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`
         return void this.client.sendMessage(M.from, { url: bnh }, MessageType.video, {quoted:M.WAMessage,
